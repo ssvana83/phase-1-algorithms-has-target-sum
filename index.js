53658c60b4d0981564e6fd536d4bd95577e334db
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-09-20 10:51:36 -0400

    commit tree 56706967b4da2658499062e4a019ab39b74b0044

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //iterate through the array with a for loop
+  for (let i = 0; i < array.length; i++) {
+  //for current num, identify compliment that adds to target(comp=target-num)
+  const complement = target - array[i];
+  //iterate through the rest of the array
+  for (let j = i + 1; j < array.length; j++) {
+  //check if any number is our complement
+    if(array[j] === complement) return true;
+  }
+  }
+  //if I reach teh end of the array, return false
+  return false;
 }
 
 /* 
@@ -8,10 +19,26 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  hasTargetSum([22, 19, 4, 6, 30], 25);
+// returns true, since 19 and 6 add up to 25
+iterate through each number of this array.
+  for the current number, identify a compliment that adds to the target (comp = target - num)
+  iterate through the reet of the array
+  check if any number is our compliment
+  if so, return true
+
+  if I get to the end of the array, and nothing adds up correctly, return false
 */
 
 /*
   Add written explanation of your solution here
+  make a function called 'hasTargetSum' that checks if two numbers from the array add up to a 
+  certain target number. for example, if i have an array [1,2,3,4] and the target is [6]
+  i should return true because 2 + 4 = 6. if i had that same array and the target was 10, it 
+  would return false. 
+
+  for each number, check if there is another number that adds to the target
+
 */
 
 // You can run `node index.js` to view these console logs
